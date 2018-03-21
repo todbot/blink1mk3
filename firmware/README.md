6b924ef7f47ac51b0efafe86ad384fd6dae918fa
Tod E. Kurt <tod@todbot.com>
2018-03-20 22:38:17 -0700

    first commit of kinda working blink1mk3 firmware

---
+++ firmware/README.md
@@ -0,0 +1,41 @@
+# tomu-samples
+
+## What are these:
+
+These projects are simple programs for the [Tomu](http://tomu.im). They do
+various things, and are intended to offer code examples and simple testing to
+ease your introduction to the Tomu platform.
+
+## How to use:
+
+1. To compile these, you'll need a cross-compiling toolchain to arm-none-eabi.
+
+  - Debian/Ubuntu/... : `sudo apt-get install gcc-arm-none-eabi`
+  - Fedora : `sudo yum install arm-none-eabi-gcc-cs arm-none-eabi-newlib`
+  - Arch : `sudo pacman -S arm-none-eabi-gcc arm-none-eabi-newlib`
+  - Other Linux : check your package manager, or
+  - Anything else (Windows, OSX, Linux) : [https://developer.arm.com/open-source/gnu-toolchain/gnu-rm ](https://developer.arm.com/open-source/gnu-toolchain/gnu-rm )
+
+  If the cross compiling toolchain is in your PATH then the Makefile will
+  auto-detect it; else (or to override it) specify it using the environment
+  variable "CROSS_COMPILE".
+  There might be additional dependencies, depending on which specific project
+  you're using, and the instructions for compilation are included in each
+  project's README.md
+
+2. Run `make deps .` to patch the linker with support for the Tomu and clone the [Gecko SDK](https://github.com/SiliconLabs/Gecko_SDK) locally
+
+3. Now you can build the examples
+
+## Flashing:
+
+Once you've compiled the code, you can flash the Tomu using `minicom` on linux
+and OSX, or some other serial I/O utility on Windows. To do that, set it up with
+`115200 8n1`, then use `i` to show the bootloader version.
+
+To upload the binary, press `u`, wait till the bootloader replies with `Ready`,
+then press `^A-S`, choose XMODEM, choose the `.bin` file produced by the
+compilation, and wait until it's uploaded.
+
+Once that's done, press `b` to boot into the firmware. The bootloader will hang
+for 5-7 seconds, then the Tomu will boot.
---
+++ firmware/blink1mk3-test1/descriptors.h
@@ -0,0 +1,206 @@
+/***************************************************************************//**
+ * @file descriptors.h
+ * @brief USB descriptors
+ * @author Energy Micro AS
+ * @version 1.01
+ *******************************************************************************
+ * @section License
+ * <b>(C) Copyright 2012 Energy Micro AS, http://www.energymicro.com</b>
+ *******************************************************************************
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ * 4. The source and compiled code may only be used on Energy Micro "EFM32"
+ *    microcontrollers and "EFR4" radios.
+ *
+ * DISCLAIMER OF WARRANTY/LIMITATION OF REMEDIES: Energy Micro AS has no
+ * obligation to support this Software. Energy Micro AS is providing the
+ * Software "AS IS", with no express or implied warranties of any kind,
+ * including, but not limited to, any implied warranties of merchantability
+ * or fitness for any particular purpose or warranties against infringement
+ * of any proprietary rights of a third party.
+ *
+ * Energy Micro AS will not be liable for any consequential, incidental, or
+ * special damages, or any other relief, or for any claim by any third party,
+ * arising from your use of this Software.
+ *
+ *****************************************************************************/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define VENDOR_ID		0x27B8	
+#define PRODUCT_ID		0x01ED
+#define DEVICE_VER		0x0101	/* Program version */
+
+#define REPORT_ID  1
+#define REPORT2_ID  2
+#define REPORT_COUNT 8
+#define REPORT2_COUNT 64
+
+SL_ALIGN(4)
+const char MyHIDReportDescriptor[48] SL_ATTRIBUTE_ALIGN(4) =
+{
+    0x06, 0x00, 0xff,              // USAGE_PAGE (Generic Desktop)
+    0x09, 0x01,                    // USAGE (Vendor Usage 1)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x85, REPORT_ID,               //   REPORT_ID (1)
+    0x95, REPORT_COUNT,            //   REPORT_COUNT (8)
+    0x09, 0x00,                    //   USAGE (Undefined)
+    0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
+    0xc0,                          // END_COLLECTION
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x85, REPORT2_ID,              //   REPORT_ID (2)
+    0x95, REPORT2_COUNT,            //   REPORT_COUNT (64)
+    0x09, 0x00,                    //   USAGE (Undefined)
+    0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
+    0xc0                           // END_COLLECTION
+};
+
+
+/* Device Descriptor. Refer to the USB 2.0 Specification, chapter 9.6 */
+SL_ALIGN(4)
+static const USB_DeviceDescriptor_TypeDef deviceDesc SL_ATTRIBUTE_ALIGN(4) =
+{
+  .bLength            = USB_DEVICE_DESCSIZE,            /* Size of the Descriptor in Bytes */
+  .bDescriptorType    = USB_DEVICE_DESCRIPTOR,          /* Device Descriptor type */
+  .bcdUSB             = 0x0200,                         /* USB 2.0 compliant */
+  .bDeviceClass       = 0x00,                           /* Vendor unique device */
+  .bDeviceSubClass    = 0,                              /* Ignored for vendor unique device */            
+  .bDeviceProtocol    = 0,                              /* Ignored for vendor unique device */
+  .bMaxPacketSize0    = USB_FS_CTRL_EP_MAXSIZE,                /* Max packet size for EP0 */
+  .idVendor           = VENDOR_ID,     
+  .idProduct          = PRODUCT_ID,                         /* PID */
+  .bcdDevice          = DEVICE_VER,                         /* Device Release number */
+  .iManufacturer      = 1,                              /* Index of Manufacturer String Descriptor */
+  .iProduct           = 2,                              /* Index of Product String Descriptor */
+  .iSerialNumber      = 3,                              /* Index of Serial Number String Descriptor */
+  .bNumConfigurations = 1                               /* Number of Possible Configurations */
+};
+
+
+/* This array contains the Configuration Descriptor and all
+ * Interface and Endpoint Descriptors for the device.
+ * Refer to the USB 2.0 Specification, chapter 9.6. */
+SL_ALIGN(4)
+static const uint8_t configDesc[] SL_ATTRIBUTE_ALIGN(4) =
+{
+  /*** Configuration descriptor ***/
+  USB_CONFIG_DESCSIZE,                  /* bLength              */
+  USB_CONFIG_DESCRIPTOR,                /* bDescriptorType      */
+
+  USB_CONFIG_DESCSIZE +                 /* wTotalLength (LSB)   */
+  USB_INTERFACE_DESCSIZE +
+  USB_HID_DESCSIZE +
+  (USB_ENDPOINT_DESCSIZE * NUM_EP_USED),
+
+  (USB_CONFIG_DESCSIZE +                /* wTotalLength (MSB)   */
+  USB_INTERFACE_DESCSIZE +
+  USB_HID_DESCSIZE +
+  (USB_ENDPOINT_DESCSIZE * NUM_EP_USED))>>8,
+
+  1,                                    /* bNumInterfaces       */
+  1,                                    /* bConfigurationValue  */
+  0,                                    /* iConfiguration       */
+  CONFIG_DESC_BM_RESERVED_D7,           /* bmAttrib             */
+  CONFIG_DESC_MAXPOWER_mA( 100 ),       /* bMaxPower: 100 mA    */
+
+  /*** Interface descriptor ***/
+  USB_INTERFACE_DESCSIZE,               /* bLength              */
+  USB_INTERFACE_DESCRIPTOR,             /* bDescriptorType      */
+  0,                                    /* bInterfaceNumber     */
+  0,                                    /* bAlternateSetting    */
+  NUM_EP_USED,                          /* bNumEndpoints        */
+  USB_CLASS_HID,                        /* bInterfaceClass      */
+  0,                                    /* bInterfaceSubClass   */
+  0,                                    /* bInterfaceProtocol   */
+  0,                                    /* iInterface           */
+
+  /*** HID descriptor ***/
+  USB_HID_DESCSIZE,       /* bLength               */
+  USB_HID_DESCRIPTOR,     /* bDescriptorType       */
+  0x11,                   /* bcdHID (LSB)          */
+  0x01,                   /* bcdHID (MSB)          */
+  0,                      /* bCountryCode          */
+  1,                      /* bNumDescriptors       */
+  USB_HID_REPORT_DESCRIPTOR,            /* bDecriptorType        */
+  sizeof(MyHIDReportDescriptor),      /* wDescriptorLength(LSB)*/
+  sizeof(MyHIDReportDescriptor) >> 8, /* wDescriptorLength(MSB)*/
+  
+  /*** Endpoint Descriptor (IN) ***/
+  USB_ENDPOINT_DESCSIZE,                /* bLength              */
+  USB_ENDPOINT_DESCRIPTOR,              /* bDescriptorType      */
+  EP_IN,                                /* bEndpointAddress     */
+  USB_EPTYPE_INTR,                      /* bmAttributes         */
+  USB_FS_INTR_EP_MAXSIZE, // USB_FS_BULK_EP_MAXSIZE,               /* wMaxPacketSize (LSB) */
+  0,                                    /* wMaxPacketSize (MSB) */
+  1,                                    /* bInterval            */
+  
+  /*** Endpoint Descriptor (OUT) ***/
+  USB_ENDPOINT_DESCSIZE,                /* bLength              */
+  USB_ENDPOINT_DESCRIPTOR,              /* bDescriptorType      */
+  EP_OUT,                               /* bEndpointAddress     */
+  USB_EPTYPE_INTR,                      /* bmAttributes         */
+  USB_FS_INTR_EP_MAXSIZE,  // USB_FS_BULK_EP_MAXSIZE,               /* wMaxPacketSize (LSB) */
+  0,                                    /* wMaxPacketSize (MSB) */
+  1,                                    /* bInterval            */
+   
+};
+
+const void *USBDESC_HidDescriptor = (void*)  &configDesc[USB_CONFIG_DESCSIZE + USB_INTERFACE_DESCSIZE];
+
+/* Define the String Descriptor for the device. String must be properly
+ * aligned and unicode encoded. The first element defines the language id. 
+ * Here 0x04 = United States, 0x09 = English. 
+ * Refer to the USB Language Identifiers documentation. */
+STATIC_CONST_STRING_DESC_LANGID( langID, 0x04, 0x09 );
+STATIC_CONST_STRING_DESC( iManufacturer, 'T','h','i','n','g','M');
+                          
+STATIC_CONST_STRING_DESC( iProduct     , 'b','l','i','n','k','(','1',')',' ','m','k','3',
+                                         ' ','0','0','1');
+//STATIC_CONST_STRING_DESC( iSerialNumber, '2','2','0','0','1','2','3','4' );
+
+SL_ALIGN(4)
+static uint8_t iSerialNumber[] =  { 18,USB_STRING_DESCRIPTOR,
+                                    '1',0,'2',0,'3',0,'4',0,'5',0,'6',0,'7',0,'8',0,
+                                    0,0};
+////                                len,type,bytes...,null
+
+static const void * const strings[] =
+{
+  &langID,
+  &iManufacturer,
+  &iProduct,
+  &iSerialNumber
+};
+
+/* Endpoint buffer sizes. Use 1 for Control/Interrupt
+ * endpoints and 2 for Bulk endpoints. */
+static const uint8_t bufferingMultiplier[ NUM_EP_USED + 1 ] = 
+{ 
+  1,  /* Control */
+  1,  /* interrupt */
+  1   /* interrupt */
+  //2,  /* Bulk */
+  //2   /* Bulk */
+};
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
