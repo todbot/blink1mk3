86dd9778657dcfd1e9e9655519351b9b9a25d573
Tod E. Kurt <tod@todbot.com>
2018-06-11 14:02:00 -0700

    add hsvtorgb function

--- firmware/blink1mk3-test4/color_funcs.h
+++ firmware/blink1mk3-test4/color_funcs.h
@@ -115,4 +115,73 @@ void rgb_updateCurrent(void)
 }
 
 
+// integer-only hsbtorgb
+// from: http://web.mit.edu/storborg/Public/hsvtorgb.c
+void hsbtorgb( uint8_t h, uint8_t s, uint8_t v,  uint8_t* nr, uint8_t* ng, uint8_t* nb )
+{
+  //uint8_t h = hsb[0];
+  //  uint8_t s = hsb[1];
+  //  uint8_t v = hsb[2];
+
+    unsigned char region, fpart, p, q, t;
+    uint8_t r,g,b;
+
+    if(s == 0) {          // color is grayscale 
+        r = g = b = v;
+        return;
+    }
+    
+    region = h / 43;      // make hue 0-5 
+    fpart = (h - (region * 43)) * 6; // find remainder part, make it from 0-255 
+    
+    // calculate temp vars, doing integer multiplication 
+    p = (v * (255 - s)) >> 8;
+    q = (v * (255 - ((s * fpart) >> 8))) >> 8;
+    t = (v * (255 - ((s * (255 - fpart)) >> 8))) >> 8;
+        
+    // assign temp vars based on color cone region 
+    switch(region) {
+        case 0:   r = v; g = t; b = p; break;
+        case 1:   r = q; g = v; b = p; break;
+        case 2:   r = p; g = v; b = t; break;
+        case 3:   r = p; g = q; b = v; break;
+        case 4:   r = t; g = p; b = v; break;
+        default:  r = v; g = p; b = q; break;
+    }    
+    *nr = r;
+    *ng = g;
+    *nb = b;
+}
+
+void hsv_to_rgb(uint8_t h, uint8_t s, uint8_t v,  uint8_t* r, uint8_t* g, uint8_t* b)
+{
+  uint8_t red_dest, grn_dest, blu_dest;
+  
+  if ( s == 0 ) {
+    red_dest = grn_dest = blu_dest = v;
+    return;
+  }
+  int16_t var_i = (h/16) / 42;
+  int16_t var_h = ((h*6)/16)%16;
+  int16_t var_1 = v/16 * ( 16 - s/16 );
+  int16_t var_2 = v/16 * ( 16 - ((s/16) * var_h)/16 );
+  int16_t var_3 = v/16 * ( 16 - ((s/16) * (16-var_h))/16 );
+
+  if ( var_i == 0 ) {
+    red_dest = v     ; grn_dest = var_3 ; blu_dest = var_1; }
+  else if ( var_i == 1 ) {
+    red_dest = var_2 ; grn_dest = v     ; blu_dest = var_1; }
+  else if ( var_i == 2 ) {
+    red_dest = var_1 ; grn_dest = v     ; blu_dest = var_3; }
+  else if ( var_i == 3 ) {
+    red_dest = var_1 ; grn_dest = var_2 ; blu_dest = v;     }
+  else if ( var_i == 4 ) {
+    red_dest = var_3 ; grn_dest = var_1 ; blu_dest = v;     }
+  else                   {
+    red_dest = v     ; grn_dest = var_1 ; blu_dest = var_2; }
+
+  *r = red_dest; *g = grn_dest; *b = blu_dest;
+}
+
+
 #endif
