6a1d2d4bad2633ae543017ec9b9fee31a30b4c3e
Tod E. Kurt <tod@todbot.com>
2018-12-15 10:29:03 -0800

    firmware: add debug_setup() and debug_ch() to debug.h

--- firmware/firwmare-v30x/debug.h
+++ firmware/firwmare-v30x/debug.h
@@ -14,13 +14,17 @@ void myputc ( void* p, char c) {
 }
 
 // a debug printf out the leuart
+#define dbg_setup() setupLeuart()
 #define dbg_printf(...) sprintf(dbgstr,__VA_ARGS__); write_str(dbgstr);
 #define dbg_str(s) write_str(s)
+#define dbg_ch(c) write_char(c)
 
 #else
 
+#define dbg_setup()
 #define dbg_printf(...)
 #define dbg_str(s)
+#define dbg_ch(c) 
 
 #endif
 
